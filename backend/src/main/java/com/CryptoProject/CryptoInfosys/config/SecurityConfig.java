91b126d6b0d6170e66bdf7e68fd557dfe67cc1dc
Ritesh Gupta <65335183+Ritesh9793@users.noreply.github.com>
2026-01-27 17:05:49 +0530

    Update SecurityConfig.java

--- backend/src/main/java/com/CryptoProject/CryptoInfosys/config/SecurityConfig.java
+++ backend/src/main/java/com/CryptoProject/CryptoInfosys/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://crypto-portfolio-tracker-fvi0.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
